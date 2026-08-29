1870066b07a65b527531285f0a3f08adbf16ef82
DanilaH <41333602+DanilaH@users.noreply.github.com>
2026-08-29 16:11:58 +0500

    Export query-family storage types

--- src/yandex_analytics_reaper/storage/__init__.py
+++ src/yandex_analytics_reaper/storage/__init__.py
@@ -2,6 +2,7 @@ from .identity import IdentityStore, SQLiteIdentityStore
 from .lineage import LineageRecord, LineageStore, SQLiteLineageStore
 from .metrics import MetricStore, MetricWrite, PersistedMetricObservation, SQLiteMetricStore
 from .probes import ProbeRunRecord, ProbeRunStore, SQLiteProbeRunStore
+from .query_families import QueryFamilyStore, SQLiteQueryFamilyStore
 from .raw import FilesystemRawSnapshotStore, RawSnapshotMetadata
 
 __all__ = [
@@ -14,9 +15,11 @@ __all__ = [
     "PersistedMetricObservation",
     "ProbeRunRecord",
     "ProbeRunStore",
+    "QueryFamilyStore",
     "RawSnapshotMetadata",
     "SQLiteIdentityStore",
     "SQLiteLineageStore",
     "SQLiteMetricStore",
     "SQLiteProbeRunStore",
+    "SQLiteQueryFamilyStore",
 ]

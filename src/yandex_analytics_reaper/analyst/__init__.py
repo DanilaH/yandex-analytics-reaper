c93bc6e02395f11073348360a3cada2a5d6aa429
DanilaH <41333602+DanilaH@users.noreply.github.com>
2026-08-29 22:18:36 +0500

    feat: expose analyst snapshot contracts

---
+++ src/yandex_analytics_reaper/analyst/__init__.py
@@ -0,0 +1,29 @@
+from .snapshot import (
+    ANALYST_SNAPSHOT_SPEC_VERSION,
+    AnalystComparableSetBinding,
+    AnalystComparableSetReference,
+    AnalystFeedRunBinding,
+    AnalystRawSnapshotReference,
+    AnalystRichMetadataBinding,
+    AnalystSnapshotBuilder,
+    AnalystSnapshotDeclaration,
+    AnalystSnapshotError,
+    AnalystSnapshotPayload,
+    AnalystSnapshotReport,
+    validate_analyst_snapshot_report,
+)
+
+__all__ = [
+    "ANALYST_SNAPSHOT_SPEC_VERSION",
+    "AnalystComparableSetBinding",
+    "AnalystComparableSetReference",
+    "AnalystFeedRunBinding",
+    "AnalystRawSnapshotReference",
+    "AnalystRichMetadataBinding",
+    "AnalystSnapshotBuilder",
+    "AnalystSnapshotDeclaration",
+    "AnalystSnapshotError",
+    "AnalystSnapshotPayload",
+    "AnalystSnapshotReport",
+    "validate_analyst_snapshot_report",
+]

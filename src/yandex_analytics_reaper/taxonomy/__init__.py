3160fdb391316e4a3e68ae4de35a0a8544f23dc8
DanilaH <41333602+DanilaH@users.noreply.github.com>
2026-08-29 19:34:58 +0500

    Export taxonomy label registries

--- src/yandex_analytics_reaper/taxonomy/__init__.py
+++ src/yandex_analytics_reaper/taxonomy/__init__.py
@@ -6,12 +6,24 @@ from .models import (
     SessionModel,
     SocialMode,
 )
+from .registries import (
+    DEFAULT_TAXONOMY_LABEL_REGISTRY_VERSION,
+    ControlledLabelDimension,
+    TaxonomyLabelRegistry,
+    TaxonomyLabelRegistryBundle,
+    get_taxonomy_label_registry,
+)
 
 __all__ = [
+    "ControlledLabelDimension",
     "ControlledTaxonomyDimensions",
+    "DEFAULT_TAXONOMY_LABEL_REGISTRY_VERSION",
     "GameTaxonomyDraft",
     "PresentationDimensions",
     "PrimaryGameplayArchetype",
     "SessionModel",
     "SocialMode",
+    "TaxonomyLabelRegistry",
+    "TaxonomyLabelRegistryBundle",
+    "get_taxonomy_label_registry",
 ]

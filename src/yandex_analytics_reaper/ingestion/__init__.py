eb9e358da251c68061f54da1b7c5def084d20d21
DanilaH <41333602+DanilaH@users.noreply.github.com>
2026-08-29 10:18:13 +0500

    feat: add ingestion orchestration package

---
+++ src/yandex_analytics_reaper/ingestion/__init__.py
@@ -0,0 +1,11 @@
+from .yandex_probes import (
+    PaginatedProbeResult,
+    ProbeCollectionError,
+    YandexPaginatedProbeRunner,
+)
+
+__all__ = [
+    "PaginatedProbeResult",
+    "ProbeCollectionError",
+    "YandexPaginatedProbeRunner",
+]
